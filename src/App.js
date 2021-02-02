a0fd4f7d38dee64fe47eca2c00a5de638051a34c
elisa <elisanba72@gmail.com>
2021-02-02 12:00:02 +0100

    Creo el sistema de rutas en App importando todas las paginas y creando el Layaut

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import React from "react"
+import {Layout} from "antd"
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
-
-
+//Importo todas las paginas
+import Home from './pages/home';
+import NewMovies from './pages/new-movies';
+import Popular from './pages/popular';
+import Search from './pages/search';
+import Movie from './pages/movie';
+import Error404 from './pages/error404';
 
 export default function App() {
-  return (
-    <div >
-     <h1>Estamos en app</h1>
-     
+  const {Header,Content} = Layout;
 
-    </div>
+
+
+  return (
+    <Layout>
+      <Router>
+        <Header>
+          Header....
+        </Header>
+
+        <Content>
+          <Switch>
+            <Route path="/" exact ={true}>
+              <Home/>
+            </Route>
+            <Route path="/new-movies"  exact={true}>
+              <NewMovies/>
+            </Route>
+            <Route path="/popular"  exact={true}>
+              <Popular/>
+            </Route>
+            <Route path="/search"  exact={true}>
+              <Search/>
+            </Route>
+            <Route path="/movie/: id" exact ={true}>
+              <Movie/>
+            </Route>
+            <Route path = "*" >
+              <Error404/>
+            </Route>
+
+
+          </Switch>
+
+        </Content>  
+      </Router>
+    </Layout>
   );
 }
 
 
+        
