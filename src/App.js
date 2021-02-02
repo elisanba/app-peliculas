bc2f12eb76ebd3cce006f8365a354acbda269893
elisa <elisanba72@gmail.com>
2021-02-02 16:40:17 +0100

    Creo el menu y enlazo las paginas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {Layout} from "antd"
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import MenuTop from './components/MenuTop';
 
 //Importo todas las paginas
 import Home from './pages/home';
@@ -18,39 +19,36 @@ export default function App() {
   return (
     <Layout>
       <Router>
-        <Header>
-          Header....
+        <Header style={{ zIndex: 1 }}>
+          <MenuTop />
         </Header>
 
         <Content>
           <Switch>
-            <Route path="/" exact ={true}>
-              <Home/>
+            <Route path="/" exact={true}>
+              <Home />
             </Route>
-            <Route path="/new-movies"  exact={true}>
-              <NewMovies/>
+            <Route path="/new-movies" exact={true}>
+              <NewMovies />
             </Route>
-            <Route path="/popular"  exact={true}>
-              <Popular/>
+            <Route path="/popular" exact={true}>
+              <Popular />
             </Route>
-            <Route path="/search"  exact={true}>
-              <Search/>
+            <Route path="/search" exact={true}>
+              <Search />
             </Route>
-            <Route path="/movie/: id" exact ={true}>
-              <Movie/>
+            <Route path="/movie/:id" exact={true}>
+              <Movie />
             </Route>
-            <Route path = "*" >
-              <Error404/>
+            <Route path="*">
+              <Error404 />
             </Route>
-
-
           </Switch>
-
-        </Content>  
+        </Content>
       </Router>
     </Layout>
   );
-}
+} 
 
 
         
