12cef05e45413c4bec2c4c157a2d627a6a43ea3c
elisa <elisanba72@gmail.com>
2021-02-03 18:43:47 +0100

    componente MovieList para mostrar listas creado y completo con sus estilos correspondientes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import {Row,Col} from 'antd'
 import useFetch from '../hooks/useFetch';
-import {URL_API,API} from '../utils/constants'
-
+import {URL_API,API} from '../utils/constants';
 import SliderMovies from '../components/SliderMovies';
+import MovieList from '../components/MovieList';
+
 
 export default function Home(){
     const newMovies =useFetch(
         `${URL_API}/movie/now_playing?api_key=${API}&language=es-Es&page=1`
   );
+
+  const popularMovies =useFetch(
+    `${URL_API}/movie/popular?api_key=${API}&language=es-Es&page=1`
+);
+
+
   
 return(
     <>
     <SliderMovies movies={newMovies}/>
+    <Row>
+        <Col span ={12}>
+            <MovieList  title ="Películas Populares"movies ={popularMovies}/>
+        </Col>
+        <Col span={12}>
+            ...
+        </Col>
+    </Row>
     </>
 );
 }
