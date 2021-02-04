f82e015459c39decbf271d8c192a67f8eb8c114f
elisa <elisanba72@gmail.com>
2021-02-04 16:08:46 +0100

    pintando la informacion de la pelicula en la pagina

--- src/pages/movie/movie.js
+++ src/pages/movie/movie.js
@@ -25,11 +25,62 @@ export default function Movie() {
       return <RenderMovie movieInfo ={movieInfo.result}/>
 
       function RenderMovie(props){
-          const {movieInfo:{title}}= props;
-      return(
-          <div>
-              <h1>Estamos en la pelicula{title}</h1>
-          </div>
-      )
+          const {movieInfo:{backdrop_path, poster_path}}= props;
+          const backdropPath = `https://image.tmdb.org/t/p/original${backdrop_path}`;
+
+          return (
+            <div
+              className="movie"
+              style={{ backgroundImage: `url('${backdropPath}')` }}
+            >
+              <div className="movie__dark" />
+              <Row>
+                <Col span={8} offset={3} className="movie__poster">
+                  <PosterMovie image={poster_path} />
+                </Col>
+                <Col span={10} className="movie__info">
+                  <MovieInfo movieInfo={props.movieInfo} />
+                </Col>
+              </Row>
+            </div>
+          );
+        }
+     
       }
-    }
+      function PosterMovie(props) {
+        const { image } = props;
+        const posterPath = `https://image.tmdb.org/t/p/original${image}`;
+      
+        return <div style={{ backgroundImage: `url('${posterPath}')` }} />;
+      }
+
+      function MovieInfo(props) {
+        const {
+          movieInfo: { id, title, release_date, overview, genres }
+        } = props;
+       
+
+        return (
+          <>
+          <div className = "movie__info-header">
+            <h1>
+              {title}
+              <span>{moment(release_date, "YYYY-MM-DD").format("YYYY")}</span>
+            </h1>
+            <button>Ver trailer</button>
+          </div>
+          <div className="movie__info-content">
+            <h3>General</h3>
+            <p>{overview}</p>
+
+            <h3>Generos</h3>
+            <ul>
+              {genres.map(gender =>(
+                <li key={gender.id}>{gender.name}</li>
+              ))}
+            </ul>
+
+          </div>
+          </>
+        );
+      }
